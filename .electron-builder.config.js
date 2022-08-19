d91a9fae3968b159219ef8acf031ea1623e73509
Matheus Teixeira <emersonmatheustu@gmail.com>
2022-08-19 11:23:31 -0400

    change icon and method to get current version from the package.json

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,6 +1,7 @@
 if (process.env.VITE_APP_VERSION === undefined) {
-  const now = new Date;
-  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+  // const now = new Date();
+  const package = require("./package.json");
+  process.env.VITE_APP_VERSION = package.version; //`${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
 }
 
 /**
@@ -8,13 +9,12 @@ if (process.env.VITE_APP_VERSION === undefined) {
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
+  productName: "Data7 Manager",
   directories: {
-    output: 'dist',
-    buildResources: 'buildResources',
+    output: "dist",
+    buildResources: "buildResources",
   },
-  files: [
-    'packages/**/dist/**',
-  ],
+  files: ["packages/**/dist/**"],
   extraMetadata: {
     version: process.env.VITE_APP_VERSION,
   },
