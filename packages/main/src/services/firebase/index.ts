411afcf7d1f9ff0ba7959b163b4dfb8c5a7c386f
Matheus Teixeira <emersonmatheustu@gmail.com>
2022-08-20 19:34:56 -0400

    remove temp firebase connection to a file .json

--- packages/main/src/services/firebase/index.ts
+++ packages/main/src/services/firebase/index.ts
@@ -1,12 +1,8 @@
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+const serviceAccount = require("./certificates/firebase.json");
 initializeApp({
-  credential: cert({
-    projectId: 'data7-api',
-    privateKey:
-      '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDZZBkUHlYzHmXX\nfXoSd/tRW5aMBwBM453KcNqRC7IxvJ3H5RRsbGkCAz87H+Q6L8bUQtUTE9LO26ZG\nupsOQ3x6NqE0eqLv4Nm4GZ5WM2poor33Xi1WwFJXW0tuoK45I0jL2sESXYCAGcBJ\n8Z9gXUh4ROJVAzoZvULPMze42jYkY5zWgt9r4/LNb/yDRHMLQCIJVZYnPYIFjq6p\n8xtVR5yQeT8m/JdnX4LTcoqTJ3T9DaTHo7GhgB1qXouh0xBw8eVOI9lJ6MML15yl\ndE4tHxBTeSjEorN2MlP60nebPbm3GcHZs9q7i1bkHLlrj3ui2LNhS3mCfGoJt/VI\n3RtOPQeVAgMBAAECggEAEwcgQX2M8qPU1YbFxOYRmQPDlUrA66SkrrqFYbASDykW\nRQ/iY2xzHdpQ54juvhUTzzJxBLQgfeYF6NJJOMy5ZtfEm8ZRirOLUFeVtK/YFC6N\nIstwe9O7oVUmsMPfBrX1l4+fL09V1ws/TyRCB0+YRJwAP4wCbYxTqQqE9GjNLQ64\nUlIE2ffP61v3KE154Cclwa7qPK5N5/8tXrPcL+NgJXbuo/jO+xaxKBnTVScMQt3A\nZX06j3DAdS9PTLfB25q3RhznCkC7cV2zkL43RTrgNTjMt0ftwVrGO/tFIf3xZLJf\nt3cZiQFC2TiSDY6vHUV5HR3iJ4HLuYGLh4ZR8iLTxQKBgQD5lIR8Y9eWkS0Pj7DS\nnnx0PzztCKr30GLlA32hxvGXVxECroagTqU1nFL2+p2BdRK3Gt4TCnNH36o40hz4\nYr+fCzZ+NlopwFrHo74Mf0ZUSPb2WkM7uLCt+jDM26SvN933A8XHO4VZGWJIc2EK\nkpxixPwktk4DgMXb8gLfyR7dHwKBgQDe+52FnlrZy6fdkpoW+Q7s5pViUCjVw5bs\nNl+RR4vLneudpPq0j6VM/I69bY9oPJpubiVnR/xIIkzxoCpPEL6Li9skCxpP874v\nY07PBnS/qppjUvG9v6eZtgGU+SZBhjiEG5ZaTgyh8PSje4DHnRWimGXL84fDyylN\nUAvHMxZQywKBgQDLyzQrhjNiJQVQcZqHQRaKraCIRM2mMSivrBwH37UiSNwA/pW7\njLxmfFFajuXR8dDoZy3zClN72uzaHOe/ApNJwRQsFyGkegcmgVQMTFkXNcVDAA1q\nVLMgPO1gOfHNiabbrt7ugnGugYW5d01EHtdH52WkbKBjR+FIaeNi7I0hLQKBgH9n\nGkCPEu+f6gCyU7JOiVWYBcBPdpV0lFXQV1hFnb0wNMA01D24WUUchF39LFPtVHtG\nGb7iJWX9myLvVV0pfwVyWjpE0bre1Ep1HkExOiGM03tul8rHp3YuxMaeeFo12zK4\nJHbiY1tfthd2aEUlHohxNLCMK4UWHjobQy+Eo/pzAoGAU18aUdXASmAB2e55Itup\nE2xQqUP4PMP+IHUhkbuWBw6ucixoGUaWCkDM8HYLxVgEuPMdFkg2xZD5EQEGT2L5\nIG1PUIiG5vdaIlQz+woVQBnWlLOm8RUyQDk5nvcejWQHnK6groHzlNr+gjHMukGP\nb6eRGLvEv20nUhfsijQtskQ=\n-----END PRIVATE KEY-----\n',
-    clientEmail: 'firebase-adminsdk-ir4v6@data7-api.iam.gserviceaccount.com',
-  }),
+  credential: cert(serviceAccount),
 });
 
 export default function useFirestore(
@@ -22,12 +18,13 @@ export default function useFirestore(
   ) {
     const doc_collection = db
       .collection(collection)
-      .orderBy(order_by_field, 'desc')
+      .orderBy(order_by_field, "desc")
       .limit(limit);
 
     const unsub = doc_collection.onSnapshot((data) => {
       data.docChanges().forEach((change) => {
         const data_doc = change.doc.data();
+
         onChange({
           doc_id: change.doc.id,
           doc_created_at: change.doc.createTime.toDate(),
@@ -49,7 +46,7 @@ export default function useFirestore(
     try {
       const DocById = db
         .collection(collection)
-        .where(Field, '==', data[Field])
+        .where(Field, "==", data[Field])
         .get();
 
       await db
